1b14b51e633c0c9ca095f4b1b9da81d9e72ce4a8
Mike Ellis <mike@thirty8.co.uk>
2025-04-29 13:17:40 +0100

    Update v

--- CultureObjectDisplay.php
+++ CultureObjectDisplay.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name: Culture Object Display
+ * Plugin Name: Culture Object Display plugin - MDS
  * Plugin URI: http://cultureobject.co.uk
  * Description: An extension to Culture Object to provide an archive view of objects and single object view for all themes.
- * Version: 1.0.1-alpha3.002
+ * Version: 1.0.2
  * Author: Liam Gladdy / Thirty8 Digital
  * Text Domain: culture-object-display
  * Author URI: https://github.com/lgladdy
