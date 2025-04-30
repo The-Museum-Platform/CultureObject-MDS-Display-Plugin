08495b02d7f201ead7d32d5d3cafb1b9c39e459a
Mike <mike@thirty8.co.uk>
2025-04-30 07:49:37 +0100

    Plugin desc

--- CultureObjectDisplay.php
+++ CultureObjectDisplay.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://cultureobject.co.uk
  * Description: An extension to Culture Object to provide an archive view of objects and single object view for all themes.
  * Version: 1.0.2
- * Author: Liam Gladdy / Thirty8 Digital
+ * Author: Liam Gladdy / Thirty8 Digital / The Museum Platform / Jack Barber
  * Text Domain: culture-object-display
  * Author URI: https://github.com/lgladdy
  * GitHub Plugin URI: Thirty8Digital/CultureObjectDisplay
