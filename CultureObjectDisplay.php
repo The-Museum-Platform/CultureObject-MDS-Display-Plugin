36d28c601776593e901e5bf029822b36fe0c0441
Liam Gladdy <liam@gladdy.co.uk>
2025-08-17 12:35:09 +0100

    Code standards, security, and general defensive programming changes

--- CultureObjectDisplay.php
+++ CultureObjectDisplay.php
@@ -8,7 +8,7 @@
  * Text Domain: culture-object-display
  * Author URI: https://github.com/lgladdy
  * GitHub Plugin URI: The-Museum-Platform/CultureObject-MDS-Display-Plugin
- * GitHub Branch: master
+ * GitHub Branch: main
  * License: Apache 2 License
  */
 
@@ -18,6 +18,18 @@ register_activation_hook( __FILE__, array( 'CultureObject\Display\COD', 'regener
 register_deactivation_hook( __FILE__, array( 'CultureObject\Display\COD', 'regenerate_permalinks' ) );
 $cod = new \CultureObject\Display\COD();
 
+function mds_make_links_clickable( $text ) {
+	return preg_replace_callback(
+		'!\b(https?://[^\s<>"]+)!i',
+		function ( $matches ) {
+			$url     = esc_url( $matches[1] );
+			$display = esc_html( $matches[1] );
+			return '<a href="' . $url . '">' . $display . '</a>';
+		},
+		$text
+	);
+}
+
 function mds_cos_fields() {
 	$id = get_the_ID();
 	if ( ! $id ) {
@@ -26,10 +38,6 @@ function mds_cos_fields() {
 
 	$obj       = cos_get_field( '@document' );
 	$obj_admin = cos_get_field( '@admin' );
-
-	function make_links_clickable( $text ) {
-		return preg_replace( '!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1">$1</a>', $text );
-	}
 
 	$fields = array();
 
@@ -62,40 +70,49 @@ function mds_cos_fields() {
 	}
 
 	$html  = '<style>#ct_complete_record_table{display:none;border-collapse: collapse;}#ct_complete_record_table.show{display:block;}#ct_complete_record_table tr{padding-bottom:4px;}#ct_complete_record_table tr.hidden{display:none;}#ct_complete_record_table tr th{text-align:left;font-size:14px;min-width:100px;background:#eee;}#ct_complete_record_table tr td{vertical-align:top;font-size:12px;border-bottom:1px solid #ccc;}#ct_complete_record_table tr td.no-border{border-bottom:none;}#ct_complete_record_table tr td .indent{padding-left:20px;font-style:italic;}#ct_complete_record_table table tr:has(td:last-child:empty) { display: none; }</style>';
-	$html .= '<h3 id="ct_complete_record"><a href="javascript:;">View complete record</a></h3>';
-	$html .= "<div id=\"ct_complete_record_table\" class=\"\"><table>\n\n";
+	$html .= '<h3 id="ct_complete_record"><a href="#">View complete record</a></h3>';
+	$html .= '<div id="ct_complete_record_table"><table>\n\n';
 
-	$f = fopen( plugin_dir_path( __FILE__ ) . 'spectrum-display.csv', 'r' );
+	$csv_file = plugin_dir_path( __FILE__ ) . 'spectrum-display.csv';
+	if ( ! file_exists( $csv_file ) ) {
+		return $html . '<tr><td>Configuration file not found.</td></tr></table></div>';
+	}
+
+	// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_fopen
+	$f = fopen( $csv_file, 'r' );
+	if ( ! $f ) {
+		return $html . '<tr><td>Unable to read configuration file.</td></tr></table></div>';
+	}
 
 	$i               = 0;
 	$display_section = false;
+	$section         = '';
 
 	$line = fgetcsv( $f );
 	while ( false !== $line ) {
 		if ( $i == 0 ) {
 			$html .= '<tr>';
 			foreach ( $line as $cell ) {
-				if ( $cell ) {
-					$html .= '<th>' . htmlspecialchars( $cell ) . '</th>';
+				if ( is_string( $cell ) && ! empty( $cell ) ) {
+					$html .= '<th>' . esc_html( $cell ) . '</th>';
 				}
 			}
 			$html .= "</tr>\n";
 		} else {
 			$c       = 0;
-			$row     = '';
 			$new_row = false;
 
 			foreach ( $line as $cell ) {
-				if ( $c == 0 && $cell !== '' ) {
+				if ( $c == 0 && ! empty( $cell ) ) {
 					if ( $display_section ) {
 						$html           .= $section;
 						$display_section = false;
 					}
 
-					$section = "<tr><td colspan='3'><strong>" . htmlspecialchars( $cell ) . '</strong></td></tr>';
+					$section = '<tr><td colspan="3"><strong>' . esc_html( $cell ) . '</strong></td></tr>';
 					$new_row = true;
 				} elseif ( $new_row ) {
-						$section .= "<tr><td class='no-border'></td><td>" . htmlspecialchars( $cell );
+						$section .= '<tr><td class="no-border"></td><td>' . esc_html( $cell );
 						$new_row  = false;
 				} elseif ( $c == 3 ) {
 						$field_key = $cell;
@@ -103,23 +120,23 @@ function mds_cos_fields() {
 					if ( isset( $fields[ $field_key ] ) ) {
 						if ( is_array( $fields[ $field_key ] ) ) {
 							foreach ( $fields[ $field_key ] as $value ) {
-								$section .= make_links_clickable( $value ) . ' ';
+								$section .= mds_make_links_clickable( $value ) . ' ';
 							}
 						} else {
-							$section .= make_links_clickable( $fields[ $field_key ] );
+							$section .= mds_make_links_clickable( $fields[ $field_key ] );
 						}
 						$display_section = true;
 					}
 						$section .= '</td></tr>';
 				} else {
 					if ( $c == 0 ) {
-						$section .= "<tr><td class='no-border'></td>";
+						$section .= '<tr><td class="no-border"></td>';
 					}
 					if ( $c == 1 ) {
-						$section .= '<td>' . htmlspecialchars( $cell );
+						$section .= '<td>' . esc_html( $cell );
 					}
 					if ( $c == 2 ) {
-						$section .= $cell ? '<span class="indent">' . htmlspecialchars( $cell ) . '</span></td>' : '</td>';
+						$section .= $cell ? '<span class="indent">' . esc_html( $cell ) . '</span></td>' : '</td>';
 					}
 				}
 
@@ -130,9 +147,10 @@ function mds_cos_fields() {
 		$line = fgetcsv( $f );
 	}
 
+	// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_fclose
 	fclose( $f );
 
-	$html .= "</table><p><a href=\"https://museumdata.uk/object-search/object/?pid={$obj_admin['uid']}\">View this record along with millions of others from UK museums at the Museum Data Service.</a></p></div><script type='text/javascript'>
+	$html .= '</table><p><a href="' . esc_url( 'https://museumdata.uk/object-search/object/?pid=' . rawurlencode( $obj_admin['uid'] ) ) . "\">View this record along with millions of others from UK museums at the Museum Data Service.</a></p></div><script type='text/javascript'>
     document.addEventListener('DOMContentLoaded', () => {
         const button = document.querySelector('#ct_complete_record');
         const elementToToggle = document.getElementById('ct_complete_record_table');
