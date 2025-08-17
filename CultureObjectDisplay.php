a0a876f05701011ded47145b95956bccb9ae05ef
Liam Gladdy <liam@gladdy.co.uk>
2025-08-17 12:14:33 +0100

    Apply code standards

--- CultureObjectDisplay.php
+++ CultureObjectDisplay.php
@@ -19,122 +19,118 @@ register_deactivation_hook( __FILE__, array( 'CultureObject\Display\COD', 'regen
 $cod = new \CultureObject\Display\COD();
 
 function mds_cos_fields() {
-    $id = get_the_ID();
-    if ( ! $id ) {
-        return false;
-    }
+	$id = get_the_ID();
+	if ( ! $id ) {
+		return false;
+	}
 
-    $obj = cos_get_field('@document');
-    $obj_admin = cos_get_field('@admin');
+	$obj       = cos_get_field( '@document' );
+	$obj_admin = cos_get_field( '@admin' );
 
-    function make_links_clickable($text){
-        return preg_replace('!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1">$1</a>', $text);
-    }
+	function make_links_clickable( $text ) {
+		return preg_replace( '!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1">$1</a>', $text );
+	}
 
-    $fields = array();
+	$fields = array();
 
-    foreach ( $obj['units'] as $field ) {
-        if (isset($field['type']) && isset($field['value'])) {
-            if ($field['type'] == 'spectrum/object_name') {
-                $fields['spectrum/object_name'] = ($fields['spectrum/object_name'] ?? '') . $field['value'] . '; ';
-            } else {
-                if (isset($fields[$field['type']])) {
-                    if (is_array($fields[$field['type']])) {
-                        $fields[$field['type']][] = $field['value'];
-                    } else {
-                        $fields[$field['type']] = array($fields[$field['type']], $field['value']);
-                    }
-                } else {
-                    $fields[$field['type']] = $field['value'];
-                }
+	foreach ( $obj['units'] as $field ) {
+		if ( isset( $field['type'] ) && isset( $field['value'] ) ) {
+			if ( $field['type'] == 'spectrum/object_name' ) {
+				$fields['spectrum/object_name'] = ( $fields['spectrum/object_name'] ?? '' ) . $field['value'] . '; ';
+			} else {
+				if ( isset( $fields[ $field['type'] ] ) ) {
+					if ( is_array( $fields[ $field['type'] ] ) ) {
+						$fields[ $field['type'] ][] = $field['value'];
+					} else {
+						$fields[ $field['type'] ] = array( $fields[ $field['type'] ], $field['value'] );
+					}
+				} else {
+					$fields[ $field['type'] ] = $field['value'];
+				}
 
-                if (isset($field['units']) && is_array($field['units'])) {
-                    $i = 0;
-                    foreach ($field['units'] as $sub_section) {
-                        if (isset($sub_section['type']) && isset($sub_section['value'])) {
-                            $fields[$sub_section['type']][$i] = $sub_section['value'];
-                            $i++;
-                        }
-                    }
-                }
-            }
-        }
-    }
+				if ( isset( $field['units'] ) && is_array( $field['units'] ) ) {
+					$i = 0;
+					foreach ( $field['units'] as $sub_section ) {
+						if ( isset( $sub_section['type'] ) && isset( $sub_section['value'] ) ) {
+							$fields[ $sub_section['type'] ][ $i ] = $sub_section['value'];
+							++$i;
+						}
+					}
+				}
+			}
+		}
+	}
 
-    $html = "<style>#ct_complete_record_table{display:none;border-collapse: collapse;}#ct_complete_record_table.show{display:block;}#ct_complete_record_table tr{padding-bottom:4px;}#ct_complete_record_table tr.hidden{display:none;}#ct_complete_record_table tr th{text-align:left;font-size:14px;min-width:100px;background:#eee;}#ct_complete_record_table tr td{vertical-align:top;font-size:12px;border-bottom:1px solid #ccc;}#ct_complete_record_table tr td.no-border{border-bottom:none;}#ct_complete_record_table tr td .indent{padding-left:20px;font-style:italic;}#ct_complete_record_table table tr:has(td:last-child:empty) { display: none; }</style>";
-    $html .= '<h3 id="ct_complete_record"><a href="javascript:;">View complete record</a></h3>';
-    $html .= "<div id=\"ct_complete_record_table\" class=\"\"><table>\n\n";
+	$html  = '<style>#ct_complete_record_table{display:none;border-collapse: collapse;}#ct_complete_record_table.show{display:block;}#ct_complete_record_table tr{padding-bottom:4px;}#ct_complete_record_table tr.hidden{display:none;}#ct_complete_record_table tr th{text-align:left;font-size:14px;min-width:100px;background:#eee;}#ct_complete_record_table tr td{vertical-align:top;font-size:12px;border-bottom:1px solid #ccc;}#ct_complete_record_table tr td.no-border{border-bottom:none;}#ct_complete_record_table tr td .indent{padding-left:20px;font-style:italic;}#ct_complete_record_table table tr:has(td:last-child:empty) { display: none; }</style>';
+	$html .= '<h3 id="ct_complete_record"><a href="javascript:;">View complete record</a></h3>';
+	$html .= "<div id=\"ct_complete_record_table\" class=\"\"><table>\n\n";
 
-    $f = fopen(plugin_dir_path(__FILE__) . "spectrum-display.csv", "r");
+	$f = fopen( plugin_dir_path( __FILE__ ) . 'spectrum-display.csv', 'r' );
 
-    $i = 0;
-    $displaySection = false;
+	$i              = 0;
+	$displaySection = false;
 
-    while (($line = fgetcsv($f)) !== false) {
-        if ($i == 0) {
-            $html .= "<tr>";
-            foreach ($line as $cell) {
-                if ($cell) {
-                    $html .= "<th>" . htmlspecialchars($cell) . "</th>";
-                }
-            }
-            $html .= "</tr>\n";
-        } else {
-            $c = 0;
-            $row = '';
-            $newRow = false;
+	while ( ( $line = fgetcsv( $f ) ) !== false ) {
+		if ( $i == 0 ) {
+			$html .= '<tr>';
+			foreach ( $line as $cell ) {
+				if ( $cell ) {
+					$html .= '<th>' . htmlspecialchars( $cell ) . '</th>';
+				}
+			}
+			$html .= "</tr>\n";
+		} else {
+			$c      = 0;
+			$row    = '';
+			$newRow = false;
 
-            foreach ($line as $cell) {
-                if ($c == 0 && $cell !== '') {
-                    if ($displaySection) {
-                        $html .= $section;
-                        $displaySection = false;
-                    }
+			foreach ( $line as $cell ) {
+				if ( $c == 0 && $cell !== '' ) {
+					if ( $displaySection ) {
+						$html          .= $section;
+						$displaySection = false;
+					}
 
-                    $section = "<tr><td colspan='3'><strong>" . htmlspecialchars($cell) . "</strong></td></tr>";
-                    $newRow = true;
-                } else {
-                    if ($newRow) {
-                        $section .= "<tr><td class='no-border'></td><td>" . htmlspecialchars($cell);
-                        $newRow = false;
-                    } else {
-                        if ($c == 3) {
-                            $field_key = $cell;
-                            $section .= "<td>";
-                            if (isset($fields[$field_key])) {
-                                if (is_array($fields[$field_key])) {
-                                    foreach ($fields[$field_key] as $value) {
-                                        $section .= make_links_clickable($value) . " ";
-                                    }
-                                } else {
-                                    $section .= make_links_clickable($fields[$field_key]);
-                                }
-                                $displaySection = true;
-                            }
-                            $section .= "</td></tr>";
-                        } else {
-                            if ($c == 0) {
-                                $section .= "<tr><td class='no-border'></td>";
-                            }
-                            if ($c == 1) {
-                                $section .= "<td>" . htmlspecialchars($cell);
-                            }
-                            if ($c == 2) {
-                                $section .= $cell ? '<span class="indent">' . htmlspecialchars($cell) . "</span></td>" : "</td>";
-                            }
-                        }
-                    }
-                }
+					$section = "<tr><td colspan='3'><strong>" . htmlspecialchars( $cell ) . '</strong></td></tr>';
+					$newRow  = true;
+				} elseif ( $newRow ) {
+						$section .= "<tr><td class='no-border'></td><td>" . htmlspecialchars( $cell );
+						$newRow   = false;
+				} elseif ( $c == 3 ) {
+						$field_key = $cell;
+						$section  .= '<td>';
+					if ( isset( $fields[ $field_key ] ) ) {
+						if ( is_array( $fields[ $field_key ] ) ) {
+							foreach ( $fields[ $field_key ] as $value ) {
+								$section .= make_links_clickable( $value ) . ' ';
+							}
+						} else {
+							$section .= make_links_clickable( $fields[ $field_key ] );
+						}
+						$displaySection = true;
+					}
+						$section .= '</td></tr>';
+				} else {
+					if ( $c == 0 ) {
+						$section .= "<tr><td class='no-border'></td>";
+					}
+					if ( $c == 1 ) {
+						$section .= '<td>' . htmlspecialchars( $cell );
+					}
+					if ( $c == 2 ) {
+						$section .= $cell ? '<span class="indent">' . htmlspecialchars( $cell ) . '</span></td>' : '</td>';
+					}
+				}
 
-                $c = ($c < 4) ? $c + 1 : 0;
-            }
-        }
-        $i++;
-    }
+				$c = ( $c < 4 ) ? $c + 1 : 0;
+			}
+		}
+		++$i;
+	}
 
-    fclose($f);
+	fclose( $f );
 
-    $html .= "</table><p><a href=\"https://museumdata.uk/object-search/object/?pid={$obj_admin['uid']}\">View this record along with millions of others from UK museums at the Museum Data Service.</a></p></div><script type='text/javascript'>
+	$html .= "</table><p><a href=\"https://museumdata.uk/object-search/object/?pid={$obj_admin['uid']}\">View this record along with millions of others from UK museums at the Museum Data Service.</a></p></div><script type='text/javascript'>
     document.addEventListener('DOMContentLoaded', () => {
         const button = document.querySelector('#ct_complete_record');
         const elementToToggle = document.getElementById('ct_complete_record_table');
@@ -150,5 +146,5 @@ function mds_cos_fields() {
     });
     </script>";
 
-    return $html;
+	return $html;
 }
