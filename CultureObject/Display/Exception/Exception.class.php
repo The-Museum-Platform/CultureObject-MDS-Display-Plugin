969049a1d48b8968be5a987ff2a6f5f27f73d042
Liam Gladdy <liam@gladdy.co.uk>
2025-08-17 11:01:25 +0100

    Revert "Space formatting"
    This reverts commit 7ae9f9f4232739a56ee06aa294c409fa3258d911.

--- CultureObject/Display/Exception/Exception.class.php
+++ CultureObject/Display/Exception/Exception.class.php
@@ -2,21 +2,13 @@
 
 namespace CultureObject\Display\Exception;
 
-class Exception extends \Exception
-{
- 
-}
-class ProviderException extends Exception
-{
- 
-}
+class Exception extends \Exception { }
+class ProviderException extends Exception { }
 
-class NotYetImplementedException extends Exception
-{
+class NotYetImplementedException extends Exception {
     protected $message = 'This functionality is not yet implemented';
 
-    function __construct()
-    {
-        $this->$message = __('This functionality is not yet implemented', 'culture-object-display');
+    function __construct() {
+        $this->$message = __( 'This functionality is not yet implemented', 'culture-object-display' );
     }
 }
